125a444c28a8cabfbde487637aa8cdfaac682785
ecarl1 <eric.mark.carlson@gmail.com>
2024-04-15 16:08:08 -0400

    d

---
+++ filter.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageBluerrer extends JFrame {
+    private JSlider blurSlider;
+    private JLabel imageLabel;
+    private BufferedImage orignalImage;
+    private BufferedImage newImage;
+
+    public ImageBluerrer(){
+
+    }
+
+    private void updateBlue(){
+
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            ImageBlurrer frame = new ImageBlurrer();
+            frame.setVisible(true);
+        });
+    }
+
+
+
+}
